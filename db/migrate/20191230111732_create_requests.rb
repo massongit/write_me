89bb399f773867dc1bea715623e554ca9ea3d56d
massongit <suzukimasaya428@gmail.com>
2019-12-31 21:28:32 +0900

    Fix column

--- db/migrate/20191230111732_create_requests.rb
+++ db/migrate/20191230111732_create_requests.rb
@@ -4,7 +4,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
       t.string :product_name, null: false
       t.text :description
       t.string :repository_url, null: false
-      t.boolean :is_created, default: false, null: false
+      t.boolean :is_completed, default: false, null: false
 
       t.timestamps
     end
