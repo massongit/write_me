0a46bd8f2b7b44b7b46aaaab93eaefb9b8b63843
massongit <suzukimasaya428@gmail.com>
2020-01-13 21:56:39 +0900

    Use rails 6.0

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module WritePlease
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
