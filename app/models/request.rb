4b0b009dd828d31a0fa035c6d14d8ced091c2a65
massongit <suzukimasaya428@gmail.com>
2020-01-13 21:09:52 +0900

    Add validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,8 @@
 
 class Request < ApplicationRecord
   enum status: %i[wanted complated]
-  validates :repository_url, format: /\A#{URI::regexp(%w(http https))}\z/
+
+  validates :product_name, presence: true
+  validates :repository_url, format: /\A#{URI::regexp(%w(http https))}\z/, presence: true
+  validates :status, presence: true
 end
