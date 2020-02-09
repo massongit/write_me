fc031b5d1a2c72a94e9e4244479f614e5dc57673
massongit <suzukimasaya428@gmail.com>
2020-02-09 15:58:15 +0900

    Change path

--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -9,42 +9,42 @@ class RequestsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    get requests_url
+    get requests_path
     assert_response :success
   end
 
   test 'should post create' do
-    post requests_url(locale: @locale), params: {request: @one.attributes}
+    post requests_path(locale: @locale), params: {request: @one.attributes}
     assert_redirected_to root_path(locale: @locale)
   end
 
   test 'should get new' do
-    get new_request_url
+    get new_request_path
     assert_response :success
   end
 
   test 'should get edit' do
-    get edit_request_url(id: @one.id)
+    get edit_request_path(id: @one.id)
     assert_response :success
   end
 
   test 'should get show' do
-    get request_url(id: @one.id)
+    get request_path(id: @one.id)
     assert_response :success
   end
 
   test 'should patch update' do
-    patch request_url(id: @one.id, locale: @locale), params: {request: @one.attributes}
+    patch request_path(id: @one.id, locale: @locale), params: {request: @one.attributes}
     assert_redirected_to root_path(locale: @locale)
   end
 
   test 'should put update' do
-    put request_url(id: @one.id, locale: @locale), params: {request: @one.attributes}
+    put request_path(id: @one.id, locale: @locale), params: {request: @one.attributes}
     assert_redirected_to root_path(locale: @locale)
   end
 
   test 'should delete destroy' do
-    delete request_url(id: @one.id, locale: @locale)
+    delete request_path(id: @one.id, locale: @locale)
     assert_redirected_to root_path(locale: @locale)
   end
 end
